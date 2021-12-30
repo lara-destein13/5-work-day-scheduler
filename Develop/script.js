e84109d4dd80e8bb77fa39bcb87013684f7b8468
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-29 21:52:53 -0500

    set background color

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+function initialize() {
+    console.log("hello");
+    for (var i = 0; i < 9; i++) {
+        var id = `hour-${i}`;
+        var element = document.getElementById(id);
+        element.style.backgroundColor = "green";
+    }
+}
+
+initialize();
