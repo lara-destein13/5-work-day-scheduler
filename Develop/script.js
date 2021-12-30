a2a1e1d25f59702c539b030b430b1e5902efe615
Lara Destein <laradestein@Laras-MacBook-Pro.local>
2021-12-29 22:10:07 -0500

    write js for background colors

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,25 @@
+// var hourString = moment().format('H');
+// var hour = parseInt(hourString);
+
+
+var hourNow = 11;
+
 function initialize() {
     console.log("hello");
     for (var i = 0; i < 9; i++) {
+        var hour = i + 9;
+        var backgroundColor = "";
+        if (hour < hourNow) {
+            backgroundColor = "gray";
+        } else if (hour === hourNow) {
+            backgroundColor = "red";
+        } else {
+            backgroundColor = "green";
+        }
+
         var id = `hour-${i}`;
         var element = document.getElementById(id);
-        element.style.backgroundColor = "green";
+        element.style.backgroundColor = backgroundColor;
     }
 }
 
